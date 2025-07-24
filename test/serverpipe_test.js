f0ee84f3300ec3d60d523b7eaab3713a206e669c
Nick Howes <n.howes@warwick.ac.uk>
2025-07-24 11:39:51 +0100

    URL-387 Reinstate our beloved addQsToUrl.
    It is consumed by apps, so let's keep it (without using the url module, or the URL class which only supports full URLs that have an origin)

--- test/serverpipe_test.js
+++ test/serverpipe_test.js
@@ -1,10 +1,30 @@
 import { assert } from 'chai';
-import { fetchWithCredentials } from '../lib/serverpipe.js';
+import { fetchWithCredentials, addQsToUrl } from '../lib/serverpipe.js';
 
 describe('serverpipe', () => {
 
   it('should have a fetchWithCredentials function', () => {
     assert.isFunction(fetchWithCredentials, 'fetchWithCredentials should be a function');
+  });
+
+  it('should append querystring to url with existing parameters', () => {
+    const originalUrl = '/api/test?cat=white';
+    const qs = {
+      cake: 'salted',
+      ts: 123,
+    };
+    const expected = `/api/test?cat=white&cake=salted&ts=123`;
+    assert.equal(addQsToUrl(originalUrl, qs), expected);
+  });
+
+  it('should append querystring to url without existing parameters', () => {
+    const originalUrl = '/api/test';
+    const qs = {
+      cake: 'salted',
+      ts: 123,
+    };
+    const expected = `/api/test?cake=salted&ts=123`;
+    assert.equal(addQsToUrl(originalUrl, qs), expected);
   });
 
 });
