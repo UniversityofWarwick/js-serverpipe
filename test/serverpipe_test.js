7d19153efe0a4a8bd9e608d8110a987440c7ce20
Nick Howes <n.howes@warwick.ac.uk>
2025-07-24 10:52:38 +0100

    UTL-387 Upgrade.
    The main issue was the import of the Node 'url' module. Webpack 5 stops importing polyfills for these things and they don't exist in the browser.
    
    We only used it for IE11 support, so I've removed all that and the import, as well as some minor tidying up.
    
    Added the 'files' property to package.json, so that we don't include extra junk in the published package.

--- test/serverpipe_test.js
+++ test/serverpipe_test.js
@@ -1,18 +1,10 @@
-import { addQsToUrl } from '../lib/serverpipe';
 import { assert } from 'chai';
+import { fetchWithCredentials } from '../lib/serverpipe.js';
 
 describe('serverpipe', () => {
 
-  it('append querystring to url', () => {
-
-    const originalUrl = '/api/test?cat=white';
-    const qs = {
-      cake: 'salted',
-      ts: 123,
-    };
-
-    const expected = `/api/test?cat=white&cake=salted&ts=123`;
-    assert.equal(addQsToUrl(originalUrl, qs), expected);
+  it('should have a fetchWithCredentials function', () => {
+    assert.isFunction(fetchWithCredentials, 'fetchWithCredentials should be a function');
   });
 
 });
